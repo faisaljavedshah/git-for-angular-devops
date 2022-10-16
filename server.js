db54f4006f73749af947278083af14b2893ac886
faisaljavedshah <acorelabs@gmail.com>
2022-10-16 15:55:41 +0500

    push server.js and package

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const port = process.env.PORT || 8081;
 const app = express();
 
 //Set the base path to the angular-test dist folder
-app.use(express.static(path.join(__dirname, 'dist/admin-panel-layout')));
+app.use(express.static(path.join(__dirname, '/')));
 
 //Any routes will be redirected to the angular app
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/admin-panel-layout/index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 //Starting server on port 8081
